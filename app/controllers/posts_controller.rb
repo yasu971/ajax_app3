e9cae0b394b536474692f79504c372f10fdfceed
yasu971 <yasuishii1421@gmail.com>
2021-02-22 17:33:29 +0900

    Update posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,10 +3,12 @@ class PostsController < ApplicationController
     @posts = Post.all.order(id: "DESC")
   end
 
-  def new
-  end
+  # 削除
+  # def new
+  # end
 
   def create
     Post.create(content: params[:content])
+    redirect_to action: :index
   end
 end
