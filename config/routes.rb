080649aa875a84cc0c1e7e0181e3ac00f66afe79
yasu971 <yasuishii1421@gmail.com>
2021-02-23 11:42:42 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # HTTPメソッド 'URIパターン', to: 'コントローラー名#アクション名'
-  root to: 'posts#index' # ←編集
-# get 'posts/new', to: 'posts#new' ←削除
+  root to: 'posts#index'
+  get 'posts', to: 'posts#checked'
   post 'posts', to: 'posts#create'
 end
