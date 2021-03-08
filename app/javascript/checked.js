9e619515be45536c1974dc810c6a7bd106d66376
yasu971 <yasuishii1421@gmail.com>
2021-03-08 12:37:57 +0900

    Update checked.js
    addEventListenerが重複して追加されることを回避。

--- app/javascript/checked.js
+++ app/javascript/checked.js
@@ -1,6 +1,10 @@
 function check() {
   const posts = document.querySelectorAll(".post");
   posts.forEach(function (post) {
+    if (post.getAttribute("data-load") != null) {
+      return null;
+    }
+    post.setAttribute("data-load", "true");
     post.addEventListener("click", () => {
       const postId = post.getAttribute("data-id");
       const XHR = new XMLHttpRequest();
@@ -22,5 +26,4 @@ function check() {
     });
   });
 }
-window.addEventListener("load", check);
 setInterval(check, 1000);
